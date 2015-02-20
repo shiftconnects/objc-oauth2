56ab6bec55c56dc5b9e934a947e6ccdb5b7865e7
Tom Zaworowski <tom@shiftconnects.com>
2015-02-20 14:38:24 -0800

    Initial Commit

---
+++ OAuth2/OAuth2.h
@@ -0,0 +1,19 @@
+//
+//  OAuth2.h
+//  OAuth2
+//
+//  Created by Tom Zaworowski on 2/20/15.
+//  Copyright (c) 2015 P100 OG, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OAuth2.
+FOUNDATION_EXPORT double OAuth2VersionNumber;
+
+//! Project version string for OAuth2.
+FOUNDATION_EXPORT const unsigned char OAuth2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OAuth2/PublicHeader.h>
+
+
